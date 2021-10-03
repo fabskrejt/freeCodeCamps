19428a101a8eebcb1c66130562e8f7eb10084a8c
Vladimir Shits <rav9aa@gmail.com>
2021-10-03 11:49:40 +0300

    Completed task - manipulating_Complex_Objects

---
+++ 90_manipulating_Complex_Objects.js
@@ -0,0 +1,24 @@
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Scrip",
+        "title": "Some",
+        "release_year": 2019,
+        "formats": [
+            "Mp3",
+            "Flac",
+            "LP"
+        ],
+
+    }
+];
